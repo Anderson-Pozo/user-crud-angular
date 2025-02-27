1331a4c19fad22b1f9ce20612d1a541c3a69d937
Anderson Pozo <anderson.pozo@upec.edu.ec>
2025-02-27 18:09:54 -0500

    feat: add environment configuration for development and production

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://qcqvzpwzmjactdgaonmr.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjcXZ6cHd6bWphY3RkZ2Fvbm1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1OTkyNDgsImV4cCI6MjA1NjE3NTI0OH0.hzkukdbxvILxEDABIxF-F6RhrTuMgpQzBH8FQhEtBjE'
+};
