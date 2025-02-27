a156f3a80ccd93c0887d1c88e92160a40fa392bc
Anderson Pozo <anderson.pozo@upec.edu.ec>
2025-02-27 18:07:42 -0500

    feat: update global styles, routing, and application configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) },
+  { path: '**', redirectTo: 'users' }
+];
